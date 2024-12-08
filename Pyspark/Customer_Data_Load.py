2ae38b5746bfb9e93b71fb365996d149610f996b
Suman Sourav <sumansouravus@gmail.com>
2024-12-08 10:54:12 -0700

    Delete Pyspark/Customer_Data_Load.py

--- Pyspark/Customer_Data_Load.py
+++
@@ -1,8 +0,0 @@
-# Databricks notebook source
-# Ensure df is properly initialized
-df = spark.read.csv('dbfs:/Suman/Customers_1000.csv', header=True, inferSchema=True)
-
-# COMMAND ----------
-
-# Write the DataFrame to CSV
-df.write.option("sep", ",").option("header", "true").mode("overwrite").csv('dbfs:/Suman/Files/Customer_Test')
